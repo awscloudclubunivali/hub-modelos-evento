f8f294cfac14413a140363171460a450e24d0db9
henriqzimer <henrique.zimermann@outlook.com>
2026-04-06 22:57:46 +0000

    subindooooooo

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
-const isProduction = process.env.NODE_ENV === "production";
-
 const nextConfig: NextConfig = {
-  output: "export",
-  trailingSlash: true,
-  basePath: isProduction ? "/hub-modelos-evento" : "",
-  images: {
-    unoptimized: true,
-  },
+ 
 };
 
 export default nextConfig;
